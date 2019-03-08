46622669c81e688b83434b5f9ea64ea74f7d94ec
Aleksandra Sorokina <alexas30s@gmail.com>
2019-03-08 11:47:06 -0800

    ignoreFile

--- ignoreFile.js
+++ ignoreFile.js
@@ -1,8 +1,9 @@
 
-<script src="https://www.gstatic.com/firebasejs/5.8.6/firebase.js"></script>
-<script>
+(function(){  
+
   // Initialize Firebase
-  var config = {
+
+	var config = {
     apiKey: "AIzaSyD9xG25_0dpoqw_TkBMaPr5pGpD1kjTshU",
     authDomain: "greenpoints-6d33d.firebaseapp.com",
     databaseURL: "https://greenpoints-6d33d.firebaseio.com",
@@ -11,4 +12,5 @@
     messagingSenderId: "1018101604499"
   };
   firebase.initializeApp(config);
-</script>
+  app_firebase = firebase; 
+})();
