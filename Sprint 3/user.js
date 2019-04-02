4b1657266fb7e030d0a0f7530e18200cada78977
Kibum Park <parkkibs@gmail.com>
2019-04-02 15:47:26 -0700

    Pulled New Updates

---
+++ Sprint 3/user.js
@@ -0,0 +1,15 @@
+(function(){
+
+    var firebase =;
+
+    // for the current user
+    // create this user node in the datebase
+
+    firebase.auth().onAuthStateChanged(function(user){
+        firebase.database().ref("Users/"+user.uid).update(
+		{
+        "name":user.displayName, 
+         "email":user.email
+        });
+    });
+})()
