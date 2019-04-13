c74143b42ded34a0b0a78e6d78cf658e94ebac3d
Aleksandra Sorokina <alexas30s@gmail.com>
2019-04-12 22:03:20 -0700

    edited formatting, created style and js_files folders

---
+++ Sprint 3/js_files/userdata.js
@@ -0,0 +1,21 @@
+(function () {
+
+    var firebase = app_firebase;
+
+
+    firebase.auth().onAuthStateChanged(function (user) {
+
+        var userId = user.uid;
+        console.log(userId);
+        var dbRef = firebase.database().ref('Users/' + userId).child("point");
+        dbRef.on("value", function (snap) {
+            let out = document.getElementById("pointers");
+            points = snap.val();
+            console.log(points);
+            var textnode = document.createTextNode("Points: " + points);
+            out.appendChild(textnode);
+        });
+
+
+    });
+}());
